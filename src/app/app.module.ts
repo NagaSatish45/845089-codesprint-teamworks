111b9375ffad36ff5e38f394a1bd15e1c2903002
NagaSatish45 <nagasatish45@gmail.com>
2020-03-03 16:51:32 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlacegiftcardComponent } from './placegiftcard/placegiftcard.component';
+import { IndexComponent } from './index/index.component';
+import { VieworderComponent } from './vieworder/vieworder.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlacegiftcardComponent,
+    IndexComponent,
+    VieworderComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
